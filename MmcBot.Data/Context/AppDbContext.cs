9d80b8650308392a3fdee76a60ddef3642f13e00
ticticboooomer <kylebc-dev@outlook.com>
2024-10-04 10:46:48 +0100

    init

---
+++ MmcBot.Data/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MmcBot.Data.Model;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace MmcBot.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<CommandMacro> CommandMacros { get; init; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<CommandMacro>().ToCollection("command_macros");
+    }
+}
---
+++ MmcBot.Data/Model/CommandMacro.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MmcBot.Data.Model;
+
+public class CommandMacro
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+    
+    [BsonElement("command")]
+    public string Command { get; set; }
+    
+    [BsonElement("response")]
+    public string Response { get; set; }
+}
